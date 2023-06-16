c41474c77c669af86e7930d315a7e9c87b99066c
Cédric BRASSEUR <Cdric.brasseur@gmail.com>
2023-06-16 16:12:32 +0200

    ✨ Create warrior class with tests

--- GreatestWarrior.Tests/WarriorTests.cs
+++ GreatestWarrior.Tests/WarriorTests.cs
@@ -1,11 +1,38 @@
 namespace GreatestWarrior.Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class WarriorTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        private Warrior _warrior;
+
+        [TestInitialize]
+        public void Init()
         {
+            _warrior = new Warrior();
+        }
+
+        [TestMethod]
+        public void CreateWarrior_LevelShouldBeOne()
+        {
+            Assert.AreEqual(1, _warrior.Level);
+        }
+
+        [TestMethod]
+        public void CreateWarrior_ExperieneSHould100()
+        {
+            Assert.AreEqual(100, _warrior.Experience);
+        }
+
+        [TestMethod]
+        public void CreateWarrior_RankShouldBePushover()
+        {
+            Assert.AreEqual(Rank.Pushover, _warrior.Rank);
+        }
+
+        [TestMethod]
+        public void CreateWarrior_AchievmentsShouleBeEmpty()
+        {
+            Assert.AreEqual(0, _warrior.CountAchievments());
         }
     }
 }
---
+++ GreatestWarrior/Warrior.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreatestWarrior
+{
+    public class Warrior
+    {
+        public int Level { get; set; }
+        public int Experience { get; set; }
+        public Rank Rank { get; set; }
+        public List<string> Achievments { get; set; }
+
+        public Warrior()
+        {
+            Level = 1;
+            Experience = 100;
+            Rank = Rank.Pushover;
+            Achievments = new List<string>();
+        }
+
+        public int CountAchievments()
+        {
+            return Achievments.Count;
+        }
+    }
+}
