48cd56c93ec151abb78926e0bcdad68139f90a44
Cédric BRASSEUR <Cdric.brasseur@gmail.com>
2023-06-16 16:17:59 +0200

    ✨ Add all ranks in enum

--- GreatestWarrior/Rank.cs
+++ GreatestWarrior/Rank.cs
@@ -9,6 +9,15 @@ namespace GreatestWarrior
     public enum Rank
     {
         Pushover = 1,
-        Novice = 2
+        Novice = 2,
+        Fighter = 3,
+        Warrior = 4,
+        Veteran = 5,
+        Sage = 6,
+        Elite = 7,
+        Conqueror = 8,
+        Champion = 9,
+        Master = 10,
+        Greatest = 11,
     }
 }
